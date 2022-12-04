984ab5b31f64cc2270f43faca8a5079fd8be7464
subetter <3077533608@qq.com>
2022-12-04 21:47:36 +0800

    Initial commit

---
+++ src/main/java/com/guo_backend/GuoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guo_backend/GuoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
